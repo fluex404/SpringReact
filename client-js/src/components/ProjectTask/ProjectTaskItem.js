4b77aec9b36a417ace265026ae0f08d97b283a64
fluex404 <isaabqari@gmail.com>
2019-04-16 15:02:07 +0700

    change react-thunk with redux-thunk

---
+++ client-js/src/components/ProjectTask/ProjectTaskItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ProjectTaskItem extends Component {
+    render() { 
+        return ( 
+            <div class="card mb-1 bg-light">
+
+                <div class="card-header text-primary">
+                    ID: projectSequence
+                </div>
+                <div class="card-body bg-light">
+                    <h5 class="card-title">summary</h5>
+                    <p class="card-text text-truncate ">
+                        acceptanceCriteria
+                    </p>
+                    <a href="lasjdflkasjdf.html" class="btn btn-primary">
+                        View / Update
+                    </a>
+
+                    <button class="btn btn-danger ml-4">
+                        Delete
+                    </button>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default ProjectTaskItem;
