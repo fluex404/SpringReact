7e91c7f9b9cb8f87622386ebf6c4de7722eea46d
fluex404 <isaabqari@gmail.com>
2019-04-16 15:10:11 +0700

    fix code of {thunk} n changed to thunk

--- client-js/src/store.js
+++ client-js/src/store.js
@@ -1,7 +1,7 @@
 
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 const initialState = {};
 const middleware = [thunk];
